c467a44d393afdc1f90e32d7c972445365215d44
emilyS135 <128805884+emilyS135@users.noreply.github.com>
2024-09-20 16:00:03 +0200

    add train.py

---
+++ train.py
@@ -0,0 +1,24 @@
+import logging
+import os
+import sys
+
+import hydra
+from omegaconf import DictConfig
+
+from src import utils
+from src.pipelines import train_ltsf_pipeline
+
+log = logging.getLogger(__name__)
+
+
+@hydra.main(version_base=None, config_path="configs")
+def main(cfg: DictConfig) -> None:
+    # Applies optional utilities
+    utils.extras(cfg)
+    train_ltsf_pipeline(cfg)
+
+if __name__ == "__main__":
+    os.environ["HYDRA_FULL_ERROR"] = "1"
+    print(sys.path)
+    print(os.getcwd())
+    main()
